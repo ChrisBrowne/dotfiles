8e6b198676de09a549869db379097ea32f7775ef
Chris Browne <chris@brownehq.uk>
2026-01-09 10:18:56 +0000

    added in some bash love.

---
+++ bash/.bashrc.d/my-config.sh
@@ -0,0 +1,14 @@
+
+if [ -f /usr/share/git-core/contrib/completion/git-prompt.sh ]; then
+    . /usr/share/git-core/contrib/completion/git-prompt.sh
+    export GIT_PS1_SHOWDIRTYSTATE=true
+    export GIT_PS1_SHOWUNTRACKEDFILES=true
+    export GIT_PS1_SHOWUPSTREAM="auto"
+    export GIT_PS1_SHOWCOLORHINTS=true
+
+    # \[e[32m\] = Start Green
+    # \u@\h     = user@host
+    # \[e[0m\]  = Reset color (so the rest is normal)
+    # \W        = Current directory
+    export PS1='[\[\e[32m\]\u@\h\[\e[0m\] \W$(__git_ps1 " (%s)")]\$ '
+fi
