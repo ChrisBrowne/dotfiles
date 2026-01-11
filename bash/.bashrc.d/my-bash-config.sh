d8c2b57173dd061d7b4cb82e31a65a7153bcf076
Chris Browne <chris@brownehq.uk>
2026-01-11 20:47:01 +0000

    more fiddling with my prompt

--- bash/.bashrc.d/my-bash-config.sh
+++ bash/.bashrc.d/my-bash-config.sh
@@ -1,14 +1,14 @@
 
 __prompt_status_line() {
     local EXIT="$?"
-    local RED="\e[31m"
-    local GREEN="\e[32m"
-    local RESET="\e[0m"
+    local RED="\001\e[31m\002"
+    local GREEN="\001\e[32m\002"
+    local RESET="\001\e[0m\002"
 
     if [ $EXIT -eq 0 ]; then
-        echo -ne "${GREEN}\$${RESET}"
+        echo -ne " ${GREEN}${EXIT}${RESET}"
     else
-        echo -ne "${RED}${EXIT} \$${RESET}"
+        echo -ne " ${RED}${EXIT}${RESET}"
     fi
 }
 
@@ -19,7 +19,7 @@ if [ -f /usr/share/git-core/contrib/completion/git-prompt.sh ]; then
     export GIT_PS1_SHOWUPSTREAM="auto"
     export GIT_PS1_SHOWCOLORHINTS=true
 
-    export PS1='[\[\e[32m\]\u@\h\[\e[0m\] \[\e[34m\]\w\[\e[0m\]$(__git_ps1 " (%s)")]\n\[$(__prompt_status_line)\] '
+    export PS1='[\[\e[32m\]\u@\h\[\e[0m\] \[\e[34m\]\w\[\e[0m\]$(__git_ps1 " (%s)")]\[$(__prompt_status_line)\]\n\[\e[32m\]❯\[\e[m\] '
 fi
 
 alias "..=cd .."
